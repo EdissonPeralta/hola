ff3a3d5d93402731c7dd0efba57905dcdf1ac68a
edissonperalta <edlperaltabe@unal.edu.co>
2020-10-04 01:18:24 -0500

    todos los morteros construidos

---
+++ tesis/retrodispersion/morteros/m3/gMC/build/CMakeFiles/gMC.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gMC.dir/gMC.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCActionInitialization.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCDetectorConstruction.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCEmCalorimeterHit.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCEmCalorimeterSD.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCEventAction.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/gMC.dir/src/gMCRunAction.cc.o"
+  "gMC"
+  "gMC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gMC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
